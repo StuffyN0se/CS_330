b4f118004f215017342cf90b4588aab2ecd6e9df
StuffyN0se <150478662+StuffyN0se@users.noreply.github.com>
2024-02-25 07:27:42 +0000

    commit

--- Assignment 4/asgn4.c
+++ Assignment 4/asgn4.c
@@ -3,14 +3,33 @@
 
 int main() {
 
-int test[] = {3,4,6,78,1,55,87};
-// int size = sizeof(test);
+    int sample;
+    FILE *file;
+    file = fopen("sample_list.txt", "r");
+    
+    if (file == NULL){
+        printf("Cannot open file.\n");
+    }
+    else {
 
-insertionSort(test, 7 );
+        while(fscanf(file, "%d", &sample) == 1){
+            printf("Number: %d \n", sample);
+            return 1;
+        }
+        fclose(file);
+    }
 
-for(int i = 0; i< 7; i++){
-    printf("a[%d] = %d\n", i, test[i]);
-}
+    int test[] = {3,4,6,78,1,55,87};
+    int size = *(&test + 1) - test;
 
+
+    // insertionSort(test, size );
+    // selectionSort(test, size);
+
+    for(int i = 0; i< size; i++){
+        printf("a[%d] = %d\n", i, *(test + i));
+    }
+
+return 0;
 
 }
--- Assignment 4/asgn4.h
+++ Assignment 4/asgn4.h
@@ -7,21 +7,40 @@ we haven't already included this header*/
 /* ========================================================================= */
 /* ~~~~~~~~~~~~~~~~~~~~~ your functions go here ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* Note: main() goes in the asgn4.c file                                     */
+
 void insertionSort(int arr[], int size){
-    for (int i = 0; i < size; i++){
-        int key = arr[i];
-        int j = i + 1;
-        while (j >= 0 && arr[i] > key) {
-            // int temp = arr[j + 1];
-            arr[j + 1] = arr[j];
-            // arr[j] = temp;
+    for (int i = 1; i < size; i++){
+        int key = *(arr + i);
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+
+            arr[j + 1] = *(arr + j);
             j -= 1;
         }
         arr[j + 1] = key;
-        
     }
 
 }
+
+void selectionSort(int array[], int size)
+    {
+        for(int i = 0; i < size -1; i++){
+            int minValue = i;
+
+            for (int j = i + 1; j < size; j++){
+                if (*(array + j)  < *(array + minValue))
+                    minValue = j;
+                if (minValue != i)
+                    {
+                        int temp = *(array + i);
+                        array[i] = *(array + minValue);
+                        array[minValue] = temp;
+                    }
+            }
+        }
+    }
+
 
 
 
